8d80de48cc9608fd739593d170f4dcade1551167
VictorGamer072YT <victorgamer0720yt@gmail.com>
2021-03-10 17:35:53 -0300

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -3,13 +3,12 @@ local client = discordia.Client()
 
 
 
-
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
 
-
+-- Set's the Status
 
 
 client:on('messageCreate', function(message)
@@ -27,12 +26,72 @@ client:on('messageCreate', function(message)
 		message.channel:send('Ping! <:doccursed:818668428625182810>')
 	end
 end)
+
+
+-- ban command, stolen example :D
+client:on('messageCreate', function(message)
+    if message.author.bot then return end
+  
+    if message.content:sub(1, 4) == '!ban' then
+      local author = message.guild:getMember(message.author.id)
+      local member = message.mentionedUsers.first
+  
+      if not member then
+        message:reply("You forgot to mention someone to ban, butthead. <:chivo:818669767043842070> <:chivo:818669767043842070> <:chivo:818669767043842070>")
+        return
+      elseif not author:hasPermission("banMembers") then
+        -- The user does not have enough permissions
+        message:reply("You can't ban people, are you dumb or just tony? <:chivo:818669767043842070> <:chivo:818669767043842070> <:chivo:818669767043842070>")
+        return
+      end
+  
+      for user in message.mentionedUsers:iter() do
+        member = message.guild:getMember(user.id)
+        if author.highestRole.position > member.highestRole.position then
+          --member:ban()
+          message:reply("Was banned! <:wheajajahat:818644418512158723>")
+          member:ban()
+        end
+      end
+    end
+  end)
+
+
+
+  client:on('messageCreate', function(message)
+
+    -- bots cant use it
+    if message.author.bot then return end
+    if message.content == '!debug' then
+    --regular plebs cant use it
+    local author = message.guild:getMember(message.author.id)
+    print('Message Author was' .. author)
+    if author ~= 496423458524102665 then
+    message.channel:send('You can\'t use that, butthead!') 
+    else
+        if author == 496423458524102665 then
+        message.channel:send('Random debug command goes here, working on megachivo, youll get 3 chivos now <:chivo:818669767043842070> <:chivo:818669767043842070> <:chivo:818669767043842070>')
+	end
+end
+end
+end)
+
+
+
+-- debug clipboard, useless
 -- <:doccursed:818668428625182810>
+-- <:chivo:818669767043842070>
+-- <:wheajajahat:818644418512158723>
+
+
+
+
 
 -- debug joke :P
 client:on('messageCreate', function(message)
     if message.author.bot then return end
-	if message.content == '<:doccursed:818668428625182810>' then
+    if message.content:sub(1, 4) == '<:doccursed:818668428625182810>' then
+	--if message.content == '<:doccursed:818668428625182810>' then
 		message.channel:send('<:doccursed:818668428625182810>')
 	end
 end)
