8d2e1bcd9120da77ea7834b7b9d8e334bff12f92
VictorGamer072YT <victorgamer0720yt@gmail.com>
2021-03-10 16:21:01 -0300

    Code cleanup, final JavaScript version.

--- bot.js
+++ bot.js
@@ -2,31 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prettyMilliseconds = require("pretty-ms");
 
-
 client.on("ready", () => {
-  console.log("T minus 10");
-  console.log("T minus 9");
-  console.log("T minus 8");
-  console.log("T minus 7");
-  console.log("T minus 6");
-  console.log("T minus 5");
-  console.log("Ignition Sequence Start");
-  console.log("T minus 4");
-  console.log("T minus 3");
-  console.log("T minus 2");
-  console.log("T minus 1");
-  console.log("T minus 0");
   console.log("WE HAVE A LIFTOFF!");
+  console.log("");
 });
-
-
-
-
-
-
-
-
-
 
 client.on("message", (message) => {
   if (message.content.startsWith("ping")) {
@@ -49,20 +28,16 @@ client.on('message', message => {
   }
 });
 
-
 client.on("guildMemberAdd", (member) => {
     if (Date.now() - member.user.createdAt <= 600000) {
         member.ban({ days: 7, reason: 'New account, suspected to be tony!' })
     }
 });
 
-
-
 client.on("message", (message) => {
   if (message.content.startsWith("!uptime")) {
     message.channel.send(`Uptime: ${prettyMilliseconds(client.uptime)}`)
   }
 });
-
 
 client.login("Njk4NTg0MzQ5NjgyOTU4NDE4.XpH9YA.5gxE856qYiXICsrD_yM0_ywYftc");
